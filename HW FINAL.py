5b34b3f59e6e843cfb653cde9314349b824505e2
jassefa <59900725+jassefa@users.noreply.github.com>
2020-02-07 16:12:17 -0600

    update

---
+++ HW FINAL.py
@@ -0,0 +1,37 @@
+
+import csv
+hw_file = open("test-election_data.csv", "r")
+file_output = ("test-election_data.csv.text")
+reader = csv.reader(hw_file)
+header = next(reader)
+number_of_votes = 0
+candidate_list = []
+percent_vote_per = {}
+num_per_Can = {}
+winner = ""
+winning_count = 0 
+
+for row in reader:
+#Number of votes cast *
+    number_of_votes = number_of_votes + 1
+    name = row[2]
+    if name not in candidate_list:
+#A complete list of candidates who received vote*
+        candidate_list.append(name)
+#track each candidates votes*
+        num_per_Can[name] = 0
+#The total number of votes each candidate won*
+    num_per_Can[name] = num_per_Can[name] + 1
+#The percentage of votes each candidate won
+for candidate in num_per_Can:
+    votes = num_per_Can.get(candidate)
+    percent_vote_per = float(votes) / float(number_of_votes) * 100
+#The winner of the election based on popular vote*
+    if (votes > winning_count):
+        winning_count = votes
+        winner = candidate
+
+
+
+    
+
--- HW_3.py
+++ HW_3.py
@@ -1,13 +1,31 @@
+import os
+import csv
 hw_file = open("test-election_data.csv", "r")
-print(hw_file.read())
+reader = csv.reader(hw_file)
+header = next(reader)
+number_of_votes = 0
+candidate_options = []
+percent_vote_per = {}
+num_per_Can = 0 
+
+for row in reader:
 #Number of votes cast 
-number_of_votes = 
-
+    number_of_votes = number_of_votes + 1
+    name = row[2]
+    print(name)
+#print(number_of_votes)
+#print("Total Votes: " + str(number_of_votes))
 #List of candidate options
-
-#The percentage of votes each candidate won
-
+    if name not in candidate_options:
+        candidate_options.append(name)
+        candidate_options[name] = candidate_options[name] + 1
+print(candidate_options)
 #The total number of votes each candidate won
-
+#        percent_vote_per[name] = 0 
+#        percent_vote_per[name] = percent_vote_per[name] + 1
+#for name in percent_vote_per:
+#    votes = percent_vote_per.get(name)
+#The percentage of votes each candidate won
+#    vote_percent = float(votes) / float(number_of_votes) * 100
+#    print(vote_percent)
 #The winner of the election based on popular vote.
-hw_file.close()
